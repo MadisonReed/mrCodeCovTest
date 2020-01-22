9ef1e28dee8c9fa85b5766d8f3cab1e8b868c9a9
Winston Yee <winsyee@gmail.com>
2020-01-22 15:37:48 -0800

    Add uncovered code to cart core

--- mr_modules/cart/cart.js
+++ mr_modules/cart/cart.js
@@ -1,5 +1,10 @@
 exports.addItems = addItems;
+exports.removeItems = removeItems;
 
 function addItems(cartObj, newItems) {
   return Object.assign(cartObj, newItems);
+}
+
+function removeItems(carObj, removeItems) {
+  return 'I remove items'
 }
