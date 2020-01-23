a7c24b930b61405cad2b521cd95197ee23e17c08
Winston Yee <winsyee@gmail.com>
2020-01-22 21:43:02 -0800

    Remove code to cart core to see if pr comment updates

--- mr_modules/cart/cart.js
+++ mr_modules/cart/cart.js
@@ -1,16 +1,5 @@
 exports.addItems = addItems;
-exports.removeItems = removeItems;
-exports.logItems = logItems;
 
 function addItems(cartObj, newItems) {
   return Object.assign(cartObj, newItems);
-}
-
-function removeItems(carObj, removeItems) {
-  return 'I remove items'
-}
-
-function logItems(cartObj) {
-  console.log(cartObj);
-  return cartObj;
 }
